b5675fc5bf11a533be0d0dd509d2d77d63e73b54
vcontreras <contrerastrece@gmail.com>
2024-03-13 16:34:51 -0500

    agregando texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,46 @@
-
 import { Navbar, TimeLine } from "./components";
 
 import { Contact } from "./components/Contact";
 import { Card } from "./components/Card";
-import { Zoom } from "react-awesome-reveal";
+import { Fade, Zoom } from "react-awesome-reveal";
+import { useState } from "react";
 
 function App() {
+  const projects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [visibleProjects, setVisibleProjects] = useState(4);
+
+  const showAllProjects = () => {
+    setVisibleProjects(projects.length);
+  };
   return (
     <main className="relative min-w-[320px]">
       <Navbar />
       <div className="max-w-4xl flex flex-col items-center  justify-center w-full mx-auto overflow-hidden px-5">
         {/* info */}
-        <section className="flex flex-col  py-5 gap-2 " id='home'>
-          <div className="flex gap-2 my-5">
-            <img src="" alt="" className="rounded-full w-24 h-24 bg-gray-400" />
-            <div>
-              <p className="text-2xl font-bold">Contreras Pariona </p>
-              <h4 className="text-lg ">Frontend Developer</h4>
-            </div>
+        <section className="flex flex-col  py-5 gap-2 " id="home">
+          <div className="flex gap-2 my-5  items-center">
+            <Fade direction="left">
+              <img
+                src=""
+                alt=""
+                className="rounded-full w-24 h-24 bg-gray-400"
+              />
+            </Fade>
+
+            <Fade direction="right">
+              <div className="">
+                <p className="text-2xl font-bold">Contreras Pariona, Victor </p>
+                <h4 className="text-lg ">Frontend Developer</h4>
+              </div>
+            </Fade>
           </div>
           <h3 className="text-xl font-bold">About Me</h3>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem ut officia in molestias adipisci quod officiis
-            facilis, consectetur similique animi magnam a, voluptas provident
-            iusto consequuntur cupiditate ex, libero quibusdam.
+            ¡Hola! Soy bachiller en Ingeniería de Sistemas por la UNAC y estoy
+            emocionado por comenzar mi viaje como Desarrollador Frontend. Mis
+            proyectos personales muestran mi pasión y habilidades. Siempre en
+            búsqueda de aprender más. ¡Listo para trabajar juntos y crear algo
+            increíble!
           </p>
           <h3 className="text-xl font-bold">Skills</h3>
           <div className="flex flex-wrap gap-3 justify-start  w-full ">
@@ -58,21 +74,28 @@ function App() {
           </div>
         </section>
         {/* Project */}
-        <section className="w-full" id='project'>
+        <section className="w-full" id="project">
           <h3 className="text-2xl font-semibold my-5">Project</h3>
           <div className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-4">
-            <Zoom cascade>
-              <Card />
-              <Card />
-              <Card />
-              <Card />
-            </Zoom>
-
+            {projects.slice(0, visibleProjects).map((project, index) => (
+              <Zoom cascade key={index} damping={1}>
+                <Card />
+              </Zoom>
+            ))}
           </div>
-          <div className="w-full flex my-2">
-
-            <button className="btn  mx-auto dark:dark:bg-neutral dark:text-neutral-content"> Ver más...</button>
-          </div>
+          {visibleProjects < projects.length ? (
+            <div className="w-full flex my-2">
+              <button
+                className="btn  mx-auto dark:dark:bg-neutral dark:text-neutral-content"
+                onClick={showAllProjects}
+              >
+                {" "}
+                Ver más...
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
         </section>
         {/* TimeLine */}
         <TimeLine />
@@ -82,7 +105,9 @@ function App() {
 
       {/* Footer */}
       <footer className="w-full border-opacity-50">
-        <div className="divider text-sm antialiased">Made by VContreras 2024</div>
+        <div className="divider text-sm antialiased">
+          Made by VContreras 2024
+        </div>
       </footer>
       {/* <MyRoutes /> */}
     </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from "react";
-import { IoCloseOutline, IoMoonOutline, IoReorderThreeOutline, IoSunnyOutline } from "react-icons/io5";
+import {
+  IoCloseOutline,
+  IoMoonOutline,
+  IoReorderThreeOutline,
+  IoSunnyOutline,
+} from "react-icons/io5";
 export const Navbar = () => {
-
   const getInitialTheme = () => {
     // Obtener el tema guardado en el localStorage
-    const savedTheme = localStorage.getItem('theme');
+    const savedTheme = localStorage.getItem("theme");
 
     // Verificar si el sistema operativo tiene tema oscuro activado
-    const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const prefersDarkMode =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
 
     // Definir el tema inicial basado en la información anterior
-    const initialTheme = savedTheme || (prefersDarkMode ? 'night' : 'light');
+    const initialTheme = savedTheme || (prefersDarkMode ? "night" : "light");
 
     // Guardar el tema inicial en el localStorage si no estaba guardado previamente
     if (!savedTheme) {
-      localStorage.setItem('theme', initialTheme);
+      localStorage.setItem("theme", initialTheme);
     }
 
     // Retornar el tema inicial
@@ -25,61 +31,70 @@ export const Navbar = () => {
   useEffect(() => {
     const htmlElement = document.querySelector("html");
     htmlElement.setAttribute("data-theme", theme);
-    localStorage.setItem('theme', theme)
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   const handleTheme = () => {
     const newTheme = theme === "light" ? "night" : "light";
     setTheme(newTheme);
     document.documentElement.setAttribute("data-theme", newTheme);
-    localStorage.setItem('theme', newTheme);
+    localStorage.setItem("theme", newTheme);
   };
 
   const handleNavClick = (sectionId) => {
-    const navbarHeight = document.querySelector('nav').offsetHeight;
+    const navbarHeight = document.querySelector("nav").offsetHeight;
     const section = document.getElementById(sectionId);
     const sectionPosition = section.offsetTop - navbarHeight;
-    window.scrollTo({ top: sectionPosition, behavior: 'smooth' });
+    window.scrollTo({ top: sectionPosition, behavior: "smooth" });
   };
-
+  const sections = ["home", "project", "contact"];
 
   return (
     <nav className=" navbar bg-base-100 shadow-md  h-16 p-2 flex items-center justify-between sticky top-0 z-10 ">
       <div>
-
-
         <div className="hidden md:flex">
           <div className="flex gap-4 w-full justify-end">
             <ul className="flex gap-4  text-xl font-medium ">
-              <li className="flex items-center"><button onClick={() => handleNavClick('home')}>About</button></li>
-              <li className="flex items-center"><button onClick={() => handleNavClick('project')}>Project</button></li>
-              <li className="flex items-center"><button onClick={() => handleNavClick('contact')}>Contact</button></li>
+              {sections.map((section, index) => (
+                <li key={index} className="cursor-pointer">
+                  <a onClick={() => handleNavClick(section)}>
+                    {section.charAt(0).toUpperCase() + section.slice(1)}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
 
         <div className="dropdown  dropdown-bottom">
-
-
-          <label className="btn btn-circle swap swap-rotate md:hidden " role="button" tabIndex={0}>
-
+          <label
+            className="btn btn-circle swap swap-rotate md:hidden "
+            role="button"
+            tabIndex={0}
+          >
             {/* this hidden checkbox controls the state */}
             <input type="checkbox" />
 
             {/* hamburger icon */}
-            {/* <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg> */}
-            <IoReorderThreeOutline size={30} className="swap-off fill-current" />
+            <IoReorderThreeOutline
+              size={30}
+              className="swap-off fill-current"
+            />
 
             {/* close icon */}
-            {/* <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg> */}
             <IoCloseOutline size={30} className="swap-on fill-current" />
-
-
           </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a onClick={() => handleNavClick('home')}>About</a></li>
-            <li><a onClick={() => handleNavClick('project')}>Project</a></li>
-            <li><a onClick={() => handleNavClick('contact')}>Contact</a></li>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {sections.map((section, index) => (
+              <li key={index} className="cursor-pointer">
+                <a onClick={() => handleNavClick(section)}>
+                  {section.charAt(0).toUpperCase() + section.slice(1)}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
@@ -87,7 +102,11 @@ export const Navbar = () => {
       <div className="flex gap-2 bg-slate-50 p-2 rounded-md dark:bg-neutral">
         <label className="swap swap-rotate">
           {/* this hidden checkbox controls the state */}
-          <input type="checkbox" onChange={handleTheme} checked={theme === 'dark'} />
+          <input
+            type="checkbox"
+            onChange={handleTheme}
+            checked={theme === "dark"}
+          />
 
           {/* moon icon */}
           <div className={theme === "night" ? "swap-off" : "swap-on"}>
