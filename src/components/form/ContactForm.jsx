5946995bcf228ae81de1b6d2445dcf4d1ad68974
vcontreras <contrerastrece@gmail.com>
2024-07-27 20:19:40 -0500

    mensaje enviado con exito

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 export const ContactForm = () => {
-  const [formData, setFormData] = useState({ to: "", subject: "", text: "" });
+  const [formData, setFormData] = useState({
+    email: "",
+    msg: "",
+  });
   const [isLoading, setIsLoading] = useState(false);
   const [status, setStatus] = useState("");
 
@@ -10,31 +13,26 @@ export const ContactForm = () => {
   };
 
   const handleSubmit = async (e) => {
-    const apiKey = import.meta.env.VITE_RESEND_API_KEY;
+    // const apiKey = import.meta.env.VITE_RESEND_API_KEY;
     e.preventDefault();
     setIsLoading(true);
     setStatus("Sending...");
 
     try {
-      const response = await fetch("/api", {
+      const response = await fetch("http://localhost:3000", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          'Authorization': `Bearer ${apiKey}`,
-        },
-        body: JSON.stringify({
-          from: "onboarding@resend.dev",
-          to: "contrerastrece@gmail.com",
-          subject: "MENSAJE DESDE PORFOLIO",
-          html: `<p>Congrats on sending your <strong>text:${formData.text}, ${formData.to}</strong>!</p>`,
-        }),
+        body: JSON.stringify(formData),
+        headers: { "Content-Type": "application/json" },
       });
-
       const result = await response.json();
       console.log(result);
 
       if (result.id) {
         setStatus("Email sent successfully!");
+        setFormData({
+          email: "",
+          msg: "",
+        });
       } else {
         setStatus("Failed to send email.");
       }
@@ -44,24 +42,22 @@ export const ContactForm = () => {
       setIsLoading(false);
     }
   };
-
   return (
     <form
       action=""
       className="flex flex-col gap-2 w-full max-w-md mx-auto"
       onSubmit={handleSubmit}
     >
-      {status && <p>{status}</p>}
       <label className="form-control w-full">
         <div className="label">
           <span className="label-text">Email</span>
         </div>
         <input
           type="email"
-          name="to"
+          name="email"
           placeholder="example@email.com"
           className="input input-bordered w-full"
-          value={formData.to}
+          value={formData.email}
           onChange={handleChange}
           required
         />
@@ -72,17 +68,28 @@ export const ContactForm = () => {
           <span className="label-text">Mensaje</span>
         </div>
         <textarea
-          name="text"
+          name="msg"
           className="textarea textarea-bordered h-24 w-full resize-none"
           placeholder="Escribe tu mensaje"
-          value={formData.text}
+          value={formData.msg}
           onChange={handleChange}
           required
         ></textarea>
       </label>
 
-      <button className="btn btn-primary w-full mt-3" type="submit">
-        {isLoading ? "Enviando..." : "Enviar"}
+      <button
+        className={`btn btn-primary w-full mt-3 ${
+          isLoading && "btn-disabled"
+        } `}
+        type="submit"
+      >
+        {isLoading ? (
+          <>
+            <span className="loading loading-spinner"></span>Enviando...
+          </>
+        ) : (
+          "Enviar"
+        )}
       </button>
       {status && (
         <p
